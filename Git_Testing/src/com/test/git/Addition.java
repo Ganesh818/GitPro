596f9e554ea6bbaa4fb0eef732135fc400a3b370
Ganesh <tinku.ganesh818@gmail.com>
2021-10-22 12:10:34 +0530

    Addition Program

---
+++ Git_Testing/src/com/test/git/Addition.java
@@ -0,0 +1,18 @@
+package com.test.git;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte a = 45;
+		byte b =12;
+		
+		int c= a+b;
+		
+		System.out.println(c);
+		
+		
+	}
+
+}
